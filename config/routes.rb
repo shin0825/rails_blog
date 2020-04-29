ff0adf92dee95afcd062b552873144728a4d6f05
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-04-29 23:56:06 +0900

    update routes and view-design

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'static_pages/home'
-  root 'static_pages#home'
+  root 'posts#index'
 end
